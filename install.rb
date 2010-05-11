365e9311c89b26d5fc2e5035b825421881468844
Graham Ashton <graham@effectif.com>
2010-05-11 14:53:20 +0100

    Removed ftools (using Ruby 1.9).

--- install.rb
+++ install.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-require 'ftools'
 
 template_executable_file = File.join(File.dirname(__FILE__), "application_files", "script", "oink")
 executable_file = File.expand_path("#{File.dirname(__FILE__)}/../../../script/oink")
